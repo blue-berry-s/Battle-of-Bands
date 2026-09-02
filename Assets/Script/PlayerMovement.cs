ca3f6dc7e1e6bfea034283f21a5f8f959c80e43b
Berry Shi <95601084+blue-berry-s@users.noreply.github.com>
2026-09-01 21:37:19 -0400

    Changed Movement to read new input system

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using System.Collections;
 
@@ -7,15 +8,29 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public PlayerController controller;
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
+
     public float runSpeed = 20f;
     public Animator animator;
     public bool isFrozen = false;
 
 
-    float horizontalMove = 0f;
+    private float horizontalMove;
     bool jump = false;
-    
-    
+
+    private void OnEnable()
+    {
+        if (moveAction != null) moveAction.action.Enable();
+        if (jumpAction != null) jumpAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        if (moveAction != null) moveAction.action.Disable();
+        if (jumpAction != null) jumpAction.action.Disable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,25 +40,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(animator.GetBool("isAttacking"));
         if (!animator.GetBool("isAttacking") && !animator.GetBool("isBlocking") && !animator.GetBool("isKicking") && !animator.GetBool("isHurt"))
         {
             if (isFrozen)
             {
                 unfreezePlayer();
             }
-            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            // 4. NEW MOVEMENT SYSTEM
+            // Reads Vector2 directly from your Action Asset configuration (e.g., WASD or D-Pad)
+            horizontalMove = moveAction.action.ReadValue<Vector2>().x * runSpeed;
+
+            // Note: If you only need the horizontal X axis, use: Mathf.Abs(horizontalMove.x)
             animator.SetFloat("moveSpeed", Mathf.Abs(horizontalMove));
 
-            if (Input.GetButtonDown("Jump"))
+            // 5. NEW JUMP SYSTEM
+            // WasPressedThisFrame() perfectly replaces Input.GetButtonDown()
+            if (jumpAction.action.WasPressedThisFrame())
             {
                 jump = true;
                 animator.SetBool("isJumping", true);
             }
         }
-        else if (animator.GetBool("isHurt")) {
-            if (jump) {
-                //Debug.Log("B");
+        else if (animator.GetBool("isHurt"))
+        {
+            if (jump)
+            {
                 transform.GetComponent<Rigidbody2D>().linearVelocityY = -10;
             }
         }
